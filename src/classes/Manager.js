0f33168af005f5b389d650e5b882aad50f60bcd0
Simvol <144470909+S1mvolxD@users.noreply.github.com>
2024-12-21 10:55:40 +0700

    Update Manager.js

--- src/classes/Manager.js
+++ src/classes/Manager.js
@@ -20,6 +20,7 @@ exports.Manager = class Manager extends Shoukaku {
         options.debug = options.debug || false;
         options.searchEngine = options.searchEngine?.toLowerCase()
             .replace('youtube', 'ytsearch')
+            .replace('yandexmusic', 'ymsearch')
             .replace('spotify', 'spsearch')
             .replace('soundcloud', 'scsearch')
             .replace('deezer', 'dzsearch')
